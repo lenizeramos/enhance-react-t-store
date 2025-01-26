ef153f02a94fd11cf2fc41fd6d51fc75b32fd0f1
Lenize Ramos <lenizemr@gmail.com>
2025-01-26 04:48:54 -0800

    add clickable contact email

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,28 +1,48 @@
 import React from "react";
 import { AiOutlineInstagram } from "react-icons/ai";
-import { IoPerson } from "react-icons/io5";
+import { IoPerson, IoMail } from "react-icons/io5";
 
 function Footer(props) {
   const iconSize = 28;
   return (
     <section className="footerSection">
-      <div className="socialmediaIcons">
-        <a
-          className="footerHyperlink"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.instagram.com/huh_ca/"
-        >
-          <AiOutlineInstagram size={iconSize} />
-        </a>
-        <a
-          className="footerHyperlink"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://howuniversehowls.myportfolio.com/"
-        >
-          <IoPerson size={iconSize/1.2} />
-        </a>
+      <div className="socialmediaIcons d-flex justify-content-evenly w-100">
+        <div>
+          <a
+            className="footerHyperlink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="mailto:example@example.com"
+          >
+            <IoMail size={iconSize / 1.2} />
+          </a>
+        </div>
+        <div>
+          <a
+            className="footerHyperlink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/huh_ca/"
+          >
+            <AiOutlineInstagram size={iconSize} />
+          </a>
+          <a
+            className="footerHyperlink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://howuniversehowls.myportfolio.com/"
+          >
+            <IoPerson size={iconSize / 1.2} />
+          </a>
+          <a
+            className="footerHyperlink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://howuniversehowls.myportfolio.com/"
+          >
+            <IoPerson size={iconSize / 1.2} />
+          </a>
+        </div>
       </div>
     </section>
   );
